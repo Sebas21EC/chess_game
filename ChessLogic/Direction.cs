bf2eb3ea111d45fd632133f085f91e537d17ff94
ERICK SEBASTIAN CARLOSAMA TABANGO <95940228+Sebas21EC@users.noreply.github.com>
2023-10-28 11:45:57 -0500

    Positions and directions

---
+++ ChessLogic/Direction.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLogic
+{
+    public class Direction
+    {
+        //These are static fields, they are like properties but they are shared by all instances of the class
+        public readonly static Direction North = new Direction(-1, 0);
+        public readonly static Direction South = new Direction(1, 0);
+        public readonly static Direction East = new Direction(0, 1);
+        public readonly static Direction West = new Direction(0, -1);
+        //Diagonals directions
+        public readonly static Direction NorthEast = North + East;
+        public readonly static Direction NorthWest = North + West;
+        public readonly static Direction SouthEast = South + East;
+        public readonly static Direction SouthWest = South + West;
+
+        public int RowDelta { get; set; }
+        public int ColumnDelta { get; set; }
+
+        public Direction(int rowDelta, int columnDelta)
+        {
+            RowDelta = rowDelta;
+            ColumnDelta = columnDelta;
+        }
+
+        public static Direction operator +(Direction dir1, Direction dir2)
+        {
+            return new Direction(dir1.RowDelta + dir2.RowDelta, dir1.ColumnDelta + dir2.ColumnDelta);
+        }
+
+        public static Direction operator *(int scalar , Direction dir)
+        {
+            return new Direction(scalar * dir.RowDelta, scalar * dir.ColumnDelta);
+        }
+
+    }
+}
---
+++ ChessLogic/Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLogic
+{
+    public enum Player
+    {
+        None,
+        White,
+        Black
+    }
+
+    public static class PlayerExtensions
+    {
+        public static Player Opponent(this Player player)
+        {
+            //This a switch statement, it is like an if statement but more compact
+            return player switch
+            {
+                //If player is white, return black, if player is black, return white, else return none
+                Player.White => Player.Black,
+                //If player is black, return white, if player is white, return black, else return none
+                Player.Black => Player.White,
+                //If player is none, return none
+                _ => Player.None,
+            };
+        }
+
+    }
+}
