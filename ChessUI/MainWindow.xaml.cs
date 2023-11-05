6bde78592cbe892c7da56d6043d2f7c30a8e9fcc
ERICK SEBASTIAN CARLOSAMA TABANGO <95940228+Sebas21EC@users.noreply.github.com>
2023-11-05 15:22:38 -0500

    Drawing the board

--- ChessUI/MainWindow.xaml.cs
+++ ChessUI/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ChessLogic;
 
 namespace ChessUI
 {
@@ -20,9 +21,48 @@ namespace ChessUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Image[,] pieceImages = new Image[8, 8];
+        private GameState gameState;
+
         public MainWindow()
         {
             InitializeComponent();
+            InitializeBoard();
+
+            gameState = new GameState(Player.White, Board.Initial());
+            DrawBoard(gameState.Board);
+        }
+
+        private void InitializeBoard()
+        {
+
+
+            for (int row = 0; row < 8; row++)
+            {
+                for (int column = 0; column < 8; column++)
+                {
+                    Image image = new Image();
+                    pieceImages[row, column] = image;
+
+                    PieceGrid.Children.Add(image);
+
+
+                }
+            }
+        }
+
+        private void DrawBoard(Board board) { 
+        
+            for(int row = 0;row < 8; row++)
+            {
+                   for(int column = 0; column < 8; column++)
+                {
+                    Piece piece = board[row, column];
+                    ImageSource imageSource = Images.GetImage(piece);
+                    pieceImages[row, column].Source = imageSource;
+                }   
+            }
+        
         }
     }
 }
